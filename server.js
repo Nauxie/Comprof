66153786b0440757cbdbb2384bbe727f810bb6df
Nauxie <abhinav.tumu1@gmail.com>
2019-11-24 00:55:08 -0800

    trace edits

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express')
+var graphqlHTTP = require('express-graphql')
+var { buildSchema } = require('graphql')
+
+var schema = buildSchema(`
+    type Query {
+        hello : String
+    }
+`)
+
+var root = { hello: () => 'data' }
+
+app = express()
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+)
+app.listen(4000, () => console.log('Browse to localhost:4000/graphql'))
